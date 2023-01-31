d4a02d39c61d8bdfd40319c020bf122b7110f40c
IgorDlima <screenmine@gmail.com>
2023-01-31 03:57:34 -0300

    third commit

--- index.js
+++ index.js
@@ -7,59 +7,27 @@ canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7
-class Sprite {
-  constructor({ position, velocity, color = 'red', offset }) {
-    this.position = position
-    this.velocity = velocity
-    this.width = 50
-    this.height = 150
-    this.lastkey
-    this.attackBox = {
-      position: {
-        x: this.position.x , 
-        y: this.position.y
-      },
-      offset,
-      width: 100 ,
-      height:50 
-    }
-    this.color = color
-    this.isAttacking
-    this.health = 100
-  }
 
-  draw() {
-    c.fillStyle = this.color
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+const background = new Sprite ({
+  position: {
+    x: 0,
+    y: 0
+  },
+  imageSrc: './img/background.png'
+})
 
-    // atack box
-    if (this.isAttacking){
-    c.fillStyle = 'green'
-    c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-  }}
-
-  update() {
-    this.draw()
-    this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-    this.attackBox.position.y = this.position.y
-
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-        this.velocity.y = 0
-    } else this.velocity.y += gravity
-  }
+const shop = new Sprite ({
+  position: {
+    x: 600,
+    y: 128
+  },
+  imageSrc: './img/shop.png' ,
+  scale: 2.75,
+  framesMax: 6
   
-  attack() {
-    this.isAttacking = true
-    setTimeout (() => {
-      this.isAttacking = false
-    }, 100 )
-  }
-}
+})
 
-const player = new Sprite({
+const player = new Fighter({
   position: {
     x: 0,
     y: 0
@@ -71,9 +39,35 @@ const player = new Sprite({
   offset: {
     x: 0,
     y: 0
+  },
+  imageSrc:'./img/samuraiMack/Idle.png' , 
+  framesMax : 8,
+  scale: 2.5,
+  offset: {
+    x: 215 ,
+    y: 157
+  },
+  sprites: {
+    idle: {
+      imageSrc: './img/samuraiMack/Idle.png' ,
+      framesMax: 8
+    },
+    run: {
+      imageSrc: './img/samuraiMack/Run.png' ,
+      framesMax: 8 
+    },
+    jump: {
+      imageSrc: './img/samuraiMack/Jump.png' ,
+      framesMax: 2 
+    },
+    fall: {
+      imageSrc: './img/samuraiMack/Fall.png' ,
+      framesMax: 2 
+    }
   }
+  
 })
-const enemy = new Sprite({
+const enemy = new Fighter({
   position: {
     x: 400,
     y: 100
@@ -109,38 +103,43 @@ const keys = {
   }
 }
 
-function rectangularCollision({ rectangle1,rectangle2 }) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
-    rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-    rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && 
-    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height 
-  )
-}
-  
-
+decreaseTimer()
 
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
+  background.update()
+  shop.update()
   player.update()
-  enemy.update()
+  // enemy.update()
 
   player.velocity.x = 0
   enemy.velocity.x = 0
 
 // player movement
+
   if (keys.a.pressed && player.lastkey === 'a') {
       player.velocity.x = -5
+      player.switchSprite('run')
   } else if (keys.d.pressed && player.lastkey === 'd') {
     player.velocity.x = 5
+    player.switchSprite('run')
+  } else {
+    player.switchSprite('idle')
+  }
+
+  if (player.velocity.y < 0) {
+    player.switchSprite('jump')
+  } else if (player.velocity.y > 0) {
+    player.switchSprite('fall')
   }
 
 // Enemy movement
   if (keys.ArrowLeft.pressed && enemy.lastkey === 'ArrowLeft') {
     enemy.velocity.x = -5
-} else if (keys.ArrowRight.pressed && enemy.lastkey === 'ArrowRight') {
+} 
+    else if (keys.ArrowRight.pressed && enemy.lastkey === 'ArrowRight') {
     enemy.velocity.x = 5
 }
 
@@ -172,7 +171,12 @@ function animate() {
       player.health -= 20
       document.querySelector('#PlayerHealth').style.width = player.health + "%"
   }
-
+ 
+ 
+  // end game based on health
+  if (enemy.health <= 0 || player.health <= 0) {
+    determinerWinner({ player, enemy, timerId })
+  }
 }
 
 animate()
